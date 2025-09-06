7ba73fed3ef78de3a8d75154873a6a6c31226315
Armin <ar.basirian@gmail.com>
2025-09-06 20:02:18 +0200

    refactor: fix minor issue

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 // Export types
-export type {
-  Breakpoint,
-  ResponsiveOverrides,
-  BaseProps,
-  CSSProps,
-  BoxProps,
-} from "./types";
+export type { Breakpoint, BaseProps, CSSProps, BoxProps } from "./types";
 
 // Export components
 export { Box } from "./components/Box";
